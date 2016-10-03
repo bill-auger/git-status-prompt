57853b7e695a34cf44e5c96896fbf01e7ac20512
bill-auger <mr.j.spam.me@gmail.com>
2016-10-02 21:43:04 -0400

    indicate 'tag' and 'commit' merge tartgets

--- git-status-prompt.sh
+++ git-status-prompt.sh
@@ -98,9 +98,9 @@ function GitStatus
 
   # detect detached HEAD state and abort
   if   [ -f $(pwd)/.git/MERGE_HEAD    ] && [ ! -z "`cat .git/MERGE_MSG | grep -E '^Merge'`" ]
-  then merge_msg=`cat .git/MERGE_MSG | grep -E "^Merge (.*)branch '"            | \
-                  sed -e "s/^Merge \(.*\)branch '\(.*\)' into \(.*\)$/\2 into \3/"`
-       echo "$UNTRACKED_COLOR(merging $merge_msg)$END"  ; return ;
+  then merge_msg=`cat .git/MERGE_MSG | grep -E "^Merge (.*)(branch|tag|commit) '"               | \
+                  sed -e "s/^Merge \(.*\)\(branch\|tag\|commit\) '\(.*\)'\( into .*\)\?$/ \3\4/"`
+       echo "$UNTRACKED_COLOR(merging$merge_msg)$END"  ; return ;
   elif [ -d $(pwd)/.git/rebase-apply/ ] || [ -d $(pwd)/.git/rebase-merge/ ]
   then rebase_dir=`ls -d .git/rebase-* | sed -e "s/^.git\/rebase-\(.*\)$/.git\/rebase-\1/"`
        this_branch=`cat $rebase_dir/head-name | sed -e "s/^refs\/heads\/\(.*\)$/\1/"`
